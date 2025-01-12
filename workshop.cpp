ad12db78766a37b84d984604b64ff72662441178
Benoît de Keyn <benoit.dekeyn@algosup.com>
2025-01-12 23:04:43 +0100

    Add some comments

--- workshop.cpp
+++ workshop.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 
 // Function to convert a string to lowercase
+// Input: string& str - the string to be converted
+// Output: None (modifies the input string in place)
 void toLowerCase(string& str) {
     transform(str.begin(), str.end(), str.begin(), [](unsigned char c) {
         return tolower(c);
@@ -16,14 +18,16 @@ void toLowerCase(string& str) {
 }
 
 class Car {
-
 public:
-    string brand;
-    int ID;
-    string state;
-    int factoryYear;
-    int price;
+    string brand; // Car brand
+    int ID; // Car ID
+    string state; // Car state (e.g., new, used)
+    int factoryYear; // Year the car was manufactured
+    int price; // Price of the car
 
+    // Constructor to initialize a Car object
+    // Inputs: carBrand, carID, carState, carFactoryYear, carPrice
+    // Output: None
     Car(string carBrand = "", int carID = 0, string carState = "", int carFactoryYear = 0, int carPrice = 0){
         this->brand = carBrand;
         this->ID = carID;
@@ -32,8 +36,10 @@ public:
         this->price = carPrice;
     }
 
+    // Function to display car details
+    // Input: title - optional title to display
+    // Output: None
     void display(string title = ""){
-        
         cout << "\n---------------------------------" << endl;
         cout << title << endl;
         cout << "-------------\n" << endl;
@@ -45,11 +51,13 @@ public:
     }
 };
 
-vector<Car> Cars;
+vector<Car> Cars; // Vector to store all cars
 
-
+// Function to display cars
+// Inputs: Car_list - list of cars to display, index - specific car index to display (optional)
+// Output: None
 void DisplayCars(vector<Car>& Car_list, int index=-1){
-    string title;
+    string title; // Title for display
     if (index == -1){
         for (int i = 0; i < Car_list.size(); i++){
             title = "Car " + to_string(i+1) + " details";
@@ -61,39 +69,36 @@ void DisplayCars(vector<Car>& Car_list, int index=-1){
     }
 }
 
+// Function to perform binary search on cars by ID
+// Inputs: Car_list - list of cars, low - lower bound, high - upper bound, searchID - ID to search for
+// Output: Index of the car if found, -1 otherwise
 int binarySearch(vector<Car>& Car_list, int low, int high, int searchID) {
-
     while (low <= high) {
-        int mid = low + (high - low) / 2;
-
+        int mid = low + (high - low) / 2; // Middle index
         if (searchID == Car_list[mid].ID){
             return mid;
         }
-
         if (searchID > Car_list[mid].ID){
             low = mid + 1;
         }
-
         if (searchID < Car_list[mid].ID){
             high = mid - 1;
         }
-
     }
-
     return -1;
-
 }
 
-
+// Function to search cars by a common string attribute
+// Inputs: attribute - pointer to the car attribute, attributeName - name of the attribute
+// Output: None
 void SearchByCommonAttribute(string Car::*attribute, string attributeName) {
-
-    string searchValue;
+    string searchValue; // Value to search for
     cout << "\nEnter the " << attributeName << " to search for: " << flush;
     cin >> searchValue;
     toLowerCase(searchValue);
 
-    vector<Car> searchResults;
-    string attributeValue;
+    vector<Car> searchResults; // Vector to store search results
+    string attributeValue; // Attribute value of the car
     for (int i = 0; i < Cars.size(); i++) {
         attributeValue = Cars[i].*attribute;
         toLowerCase(attributeValue);
@@ -105,13 +110,15 @@ void SearchByCommonAttribute(string Car::*attribute, string attributeName) {
     DisplayCars(searchResults);
 }
 
+// Function to search cars by a common integer attribute
+// Inputs: attribute - pointer to the car attribute, attributeName - name of the attribute
+// Output: None
 void SearchByCommonAttribute(int Car::*attribute, string attributeName) {
-
-    int searchValue;
+    int searchValue; // Value to search for
     cout << "\nEnter the " << attributeName << " to search for: " << flush;
     cin >> searchValue;
 
-    vector<Car> searchResults;
+    vector<Car> searchResults; // Vector to store search results
     for (int i = 0; i < Cars.size(); i++) {
         if (Cars[i].*attribute == searchValue) {
             searchResults.push_back(Cars[i]);
@@ -121,9 +128,11 @@ void SearchByCommonAttribute(int Car::*attribute, string attributeName) {
     DisplayCars(searchResults);
 }
 
+// Function to search for a car by ID
+// Inputs: None
+// Output: None
 void SearchByID() {
-
-    int searchValue;
+    int searchValue; // ID to search for
     cout << "Enter the ID to search for: " << flush;
     cin >> searchValue;
 
@@ -138,8 +147,10 @@ void SearchByID() {
     }
 }
 
+// Function to handle search menu and options
+// Inputs: None
+// Output: None
 void Search(){
-
     cout << "\n\nSelect search option" << endl;
     cout << "1. Search by brand" << endl;
     cout << "2. Search by ID" << endl;
@@ -148,10 +159,8 @@ void Search(){
     cout << "5. Search by price" << endl;
     cout << "Choose an option :" << flush;
 
-    int option;
+    int option; // User's search option
     cin >> option;
-
-    vector <Car> searchResults;
 
     switch (option)
     {
@@ -177,9 +186,11 @@ void Search(){
     }
 }
 
+// Function to delete a car by ID
+// Inputs: None
+// Output: None
 void DeleteCar(){
-
-    int searchValue;
+    int searchValue; // ID of the car to delete
     cout << "Enter the ID of the car to delete: " << flush;
     cin >> searchValue;
 
@@ -192,11 +203,13 @@ void DeleteCar(){
         Cars.erase(Cars.begin() + CarIndex);
         cout << "\nCar deleted successfully" << endl;
     }
-    
 }
 
+// Function to edit a car's details
+// Inputs: None
+// Output: None
 void EditCar(){
-    int searchValue;
+    int searchValue; // ID of the car to edit
     cout << "Enter the ID of the car to edit: " << flush;
     cin >> searchValue;
 
@@ -206,7 +219,6 @@ void EditCar(){
         cout << "\nCar not found\n" << endl;
         return;
     } else {
-        
         cout << "\nCar found !" << endl;
         cout << "Select an property to change: \n" << endl;
         cout << "1. Edit brand        : " << Cars[CarIndex].brand << endl;
@@ -216,7 +228,7 @@ void EditCar(){
         cout << "5. Edit price        : " << Cars[CarIndex].price << endl;
         cout << "\nChoose an option   :" << flush;
 
-        int option;
+        int option; // User's edit option
         cin >> option;
 
         switch (option)
@@ -251,12 +263,15 @@ void EditCar(){
     }
 }
 
+// Function to merge two halves of a car list
+// Inputs: Car_list - list of cars, lowerB - lower bound, mid - middle index, UpperB - upper bound
+// Output: None
 void merge(vector<Car>& Car_list, int lowerB, int mid, int UpperB) {
-    int sizeLeft = mid - lowerB + 1;
-    int sizeRight = UpperB - mid;
+    int sizeLeft = mid - lowerB + 1; // Size of left half
+    int sizeRight = UpperB - mid; // Size of right half
 
-    vector<Car> leftArray(sizeLeft);
-    vector<Car> rightArray(sizeRight);
+    vector<Car> leftArray(sizeLeft); // Left half array
+    vector<Car> rightArray(sizeRight); // Right half array
 
     for (int i = 0; i < sizeLeft; ++i) {
         leftArray[i] = Car_list[lowerB + i];
@@ -265,7 +280,7 @@ void merge(vector<Car>& Car_list, int lowerB, int mid, int UpperB) {
         rightArray[i] = Car_list[mid + 1 + i];
     }
 
-    int i = 0, j = 0, k = lowerB;
+    int i = 0, j = 0, k = lowerB; // Indices for merging
 
     while ((i < sizeLeft) && (j < sizeRight)) {
         if (leftArray[i].ID <= rightArray[j].ID) {
@@ -291,22 +306,26 @@ void merge(vector<Car>& Car_list, int lowerB, int mid, int UpperB) {
     }
 }
 
+// Function to perform merge sort on a list of cars
+// Inputs: Car_list - list of cars, lowerB - lower bound, upperB - upper bound
+// Output: None
 void mergeSort(vector<Car>& Car_list, int lowerB, int upperB){
-
     if (lowerB >= upperB){
         return;
     }
 
-    int mid = lowerB + (upperB - lowerB) / 2;
+    int mid = lowerB + (upperB - lowerB) / 2; // Middle index
     mergeSort(Car_list, lowerB, mid);
     mergeSort(Car_list, mid + 1, upperB);
     merge(Car_list, lowerB, mid, upperB);
 }
 
+// Function to add a new car
+// Inputs: None
+// Output: None
 void AddCar(){
+    Car temp_car; // Temporary car object
 
-    Car temp_car;
-    
     cout << "Add car's brand" << endl;
     cin >> temp_car.brand;
     cout << "Add car's ID" << endl;
@@ -317,18 +336,18 @@ void AddCar(){
     cin >> temp_car.factoryYear;
     cout << "Add car's price" << endl;
     cin >> temp_car.price;
-    cout << "Add car's mileage" << endl;
 
     Cars.push_back(temp_car);
 
     mergeSort(Cars, 0, Cars.size() - 1);
-    
-    cout << "Car added successfully" << endl;
 
+    cout << "Car added successfully" << endl;
 }
 
+// Function to preload cars into the system
+// Inputs: None
+// Output: None
 void PreloadCars(){
-
     // Preload 4 cars
     Car car1("Toyota", 1263, "New", 2021, 100000);
     Car car2("BMW", 5712, "Occasion", 2019, 200000);
@@ -341,17 +360,14 @@ void PreloadCars(){
     Cars.push_back(car4);
 
     mergeSort(Cars, 0, Cars.size() - 1);
-
 }
 
 int main() {
-
-    int option = 0;
+    int option = 0; // User's menu option
 
     PreloadCars();
 
     while (option != 6) {
-
         cout << "\n\n__________________________________________\n" << endl;
         cout << "Menu" << endl;
         cout << "1. Add car" << endl;
